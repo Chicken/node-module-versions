59cdd6852b4ebb6a5b034740db04c7f5fd011170
Antti Ellilä <antti@antti.codes>
2024-11-03 23:49:19 +0200

    init

---
+++ main.js
@@ -0,0 +1,42 @@
+document.addEventListener("DOMContentLoaded", async () => {
+    const table = /** @type {HTMLTableElement} */ (document.querySelector("table"));
+    const runtimeSelect = /** @type {HTMLSelectElement} */ (document.querySelector("select"));
+
+    const data = await fetch(
+        "https://raw.githubusercontent.com/nodejs/node/refs/heads/main/doc/abi_version_registry.json"
+    ).then((res) => res.json());
+    const nodeModuleVersions = data.NODE_MODULE_VERSION;
+
+    const runtimes = [...new Set(nodeModuleVersions.map((item) => item.runtime))];
+    runtimes.forEach((runtime) => {
+        const option = document.createElement("option");
+        option.value = runtime;
+        option.textContent = runtime;
+        runtimeSelect.appendChild(option);
+    });
+
+    function populateTable() {
+        while (table.rows.length > 1) table.deleteRow(1);
+
+        nodeModuleVersions
+            .filter((nmv) => runtimeSelect.value === "all" || nmv.runtime == runtimeSelect.value)
+            .forEach((item) => {
+                const row = table.insertRow();
+
+                const cellModules = row.insertCell();
+                cellModules.textContent = item.modules;
+
+                const cellRuntime = row.insertCell();
+                cellRuntime.textContent = item.runtime;
+
+                const cellVersions = row.insertCell();
+                cellVersions.textContent = item.versions;
+            });
+    }
+
+    populateTable();
+
+    runtimeSelect.addEventListener("change", () => {
+        populateTable();
+    });
+});
